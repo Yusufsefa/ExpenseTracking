e1f34c2ac88cc328f570d96cf9217d8ee66540a2
Yusuf Sefa ALİŞ <yusufsefa61@gmail.com>
2021-05-08 18:40:32 +0300

    add splash animation

--- app/src/main/java/com/yyusufsefa/expensetracking/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/yyusufsefa/expensetracking/ui/splash/SplashFragment.kt
@@ -1,23 +1,33 @@
-package com.yyusufsefa.expensetracking.ui
+package com.yyusufsefa.expensetracking.ui.splash
 
 import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.navigation.fragment.findNavController
 import com.yyusufsefa.expensetracking.R
 import com.yyusufsefa.expensetracking.common.BaseFragment
+import com.yyusufsefa.expensetracking.data.preferences.MyPreferences
 import com.yyusufsefa.expensetracking.databinding.FragmentSplashBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SplashFragment : BaseFragment<FragmentSplashBinding>(R.layout.fragment_splash) {
 
+    @Inject
+    lateinit var preferences: MyPreferences
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val slideAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.side_slide)
+        binding.imgSplash.startAnimation(slideAnimation)
+
         Handler(Looper.getMainLooper()).postDelayed({
-            if (onBoardingFinished()) {
+            if (preferences.getOnBoarding() && preferences.isLogin()) {
                 findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
             } else {
                 findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
